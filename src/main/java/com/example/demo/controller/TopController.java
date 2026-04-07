94bb14507cb678484088caaff92ab2377dd77405
Nobu-DD <n.s.mh256@gmail.com>
2026-04-07 20:30:16 +0900

    fix: htmlを返さない返り値に変更

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -1,17 +1,22 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 @RequestMapping("/")
 public class TopController {
 
-   @GetMapping
-   public String top() {
-       return "index";
+   @GetMapping("/")
+   public List<String> top() {
+       List<String> array = new ArrayList<>();
+       array.add("これはトップページ");
+       return array;
    }
 
    @GetMapping("comment")
